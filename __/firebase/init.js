45cd9f22d3067cfc73d0daa8106f46957e8faf11
Ben Lee <ben@brainleadercompany.com>
2025-06-18 23:25:11 +0400

    remove no longer needed files

--- __/firebase/init.js
+++
@@ -1,9 +0,0 @@
-if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
-firebase.initializeApp({
-	"apiKey": "AIzaSyBsd60iLa0xBtvR9EqcpON0kBMrLtQM_HE",
-	"databaseURL": "https://brain-leader-company.firebaseio.com",
-	"storageBucket": "brain-leader-company.appspot.com",
-	"authDomain": "brain-leader-company.firebaseapp.com",
-	"messagingSenderId": "905208705577",
-	"projectId": "brain-leader-company"
-});
